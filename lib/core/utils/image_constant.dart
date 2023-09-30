a3270570f6ef8cb189fe24b60e5b740761260968
bolaji Adeladan <bolaji.adeladan@gmail.com>
2023-09-30 12:00:46 +0100

    image and color constant implemented

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,11 @@
+class ImageConstant {
+  static const String smallAppLogo = 'assets/images/app_logo_small.png';
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String books = 'assets/images/books.png';
+  static const String cardPaymentImage = 'assets/images/card_payment_image.png';
+  static const String chatBotImage = 'assets/images/chat_bot.png';
+  static const String planCategoryImage = 'assets/images/plan_category_image.png';
+  static const String profileImage = 'assets/images/profile_image.png';
+  static const String welcomeImage1 = 'assets/images/welcome_image1.png';
+  static const String welcomeImage2 = 'assets/images/welcome_image2.png';
+}
---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFFBC7AF9);
+  static const Color blackColor = Color(0xFF000000);
+  static Color black50Color = Color(0xFF000000).withOpacity(0.5);
+  static Color black70Color = Color(0xFF000000).withOpacity(0.7);
+  static const Color textLightColor = Color(0xFFFC93C5);
+  static const Color textRedColor = Color(0xFFE92626);
+  static const Color welcomeTextColor = Color(0xFF2F2E41);
+  static const Color whiteColor = Color(0xFFffffff);
+  static const Color buttonBorderColor = Color(0xFFBBBBBB);
+  static const Color iconRedColor = Color(0xFFF67280);
+  static const Color plansContainerColor = Color(0xFFF3E6FE);
+  static const Color redColor = Color(0xFFC23535);
+}
