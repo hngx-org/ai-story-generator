0829a25855ae1d2dba0296ad2dd2cd52e0007b37
1297820 <umunubo.lg@gmail.com>
2023-10-03 14:35:44 +0100

    ai chat screen

--- lib/screens/ai_screens/ai_chat_controller.dart
+++ lib/screens/ai_screens/ai_chat_controller.dart
@@ -1,25 +1,41 @@
 import 'package:ai_story_generator/core/app_export.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/utils/progress_dialog_utils.dart';
+
 class AiChatContainer extends StatelessWidget {
-  const AiChatContainer({super.key});
+  const AiChatContainer({super.key, required this.inputText,});
+  final String inputText;
 
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppTheme.plansContainerColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: customJustifiedText(
-        inputText:
-            'Lorem ipsum dolor sit amet consectetur. Phasellus leo felis ac ut a eu. Id tincidunt scelerisque malesuada etiam elit sodales sem amet suspendisse. Tortor vitae vulputate ipsum risus dignissim adipiscing lobortis. Porttitor volutpat dolor faucibus lobortis dis orci et et. Senectus id pulvinar odio. \n\nfermentum elementum tellus ut mauris. Fringilla pellentesque lorem neque volutpat. Eu morbi dui morbi aliquam eu consequat blandit dignissim dolor. Nullam interdum sed etiam at id varius nunc. Consectetur id vitae purus. \n\nrhoncus volutpat velit ac bibendum nisi. Id ipsum amet eu sem porta auctor vestibulum. Mauris nunc enim est lorem velit velit. Justo tristique dictum euismod id eros lorem orci mauris. Aliquet in blandit nibh commodo praesent euismod a. Dolor eleifend quam tempus quispellentesque gravida. Sagittis vulputate egestas eget aliquet odio. Amet rutrum pellentesque aliquam tempus nibh. \n\nsollicitudin odio netus ac. Aenean vulputate morbi tempus gravida. Tincidunt turpis purus rutrum amet tincidunt. Ut nunc ut suspendisse nisi ut. Tellus proin rhoncus mauris a lacus. Vel viverra aliquet imperdiet eget integer netus. Commodo dignissim ultricies tellus viverra vulputate est vive tristique. Porttitor at hendrerit adipiscing phasellus facilisis vulputate. In varius cras imperdiet pretium in tellus tempus blandit imperdiet.',
-        fontSize: 16,
-        weight: FontWeight.w400,
-        colorName: AppTheme.blackColor,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          ImageConstant.chatBotImage,
+          scale: 4,
+        ),
+        Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppTheme.plansContainerColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: customJustifiedText(
+                    inputText: inputText,
+                    fontSize: 16,
+                    weight: FontWeight.w400,
+                    colorName: AppTheme.blackColor,
+                  ),
+                ),
+              )
+            
+      ],
     );
   }
 }
--- lib/screens/ai_screens/individual_ai_chat.dart
+++ lib/screens/ai_screens/individual_ai_chat.dart
@@ -1,13 +1,24 @@
 import 'package:ai_story_generator/core/app_export.dart';
+import 'package:ai_story_generator/screens/ai_screens/ai_chat_controller.dart';
+import 'package:ai_story_generator/screens/ai_screens/chat_textfield.dart';
 import 'package:ai_story_generator/screens/ai_screens/user_chat_container.dart';
 import 'package:flutter/material.dart';
 
-class IndividualAiChatScreen extends StatelessWidget {
+import '../../controller/ai_controller.dart';
+
+class IndividualAiChatScreen extends StatefulWidget {
   const IndividualAiChatScreen({super.key, required this.screenType});
   final String screenType;
 
   @override
+  State<IndividualAiChatScreen> createState() => _IndividualAiChatScreenState();
+}
+
+class _IndividualAiChatScreenState extends State<IndividualAiChatScreen> {
+  bool aiResponse = true;
+  @override
   Widget build(BuildContext context) {
+    final _aiController = Get.put(AiController());
     SizeConfig.init(context);
     return Scaffold(
       appBar: AppBar(
@@ -22,31 +33,84 @@ class IndividualAiChatScreen extends StatelessWidget {
         ),
         centerTitle: true,
         title: customNormalText(
-            inputText: screenType,
-            fontSize: 32,
-            weight: FontWeight.w800,
-            colorName: AppTheme.blackColor),
+          inputText: widget.screenType,
+          fontSize: 32,
+          weight: FontWeight.w800,
+          colorName: AppTheme.blackColor,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _aiController.aiInput.add({
+                  "ai":
+                      "Lorem ipsum dolor sit amet consectetur. Phasellus leo felis ac ut a eu. Id tincidunt scelerisque malesuada etiam elit sodales sem amet suspendisse. Tortor vitae vulputate ipsum risus dignissim adipiscing lobortis. Porttitor volutpat dolor faucibus lobortis dis orci et et. Senectus id pulvinar odio. \n\nfermentum elementum tellus ut mauris. Fringilla pellentesque lorem neque volutpat. Eu morbi dui morbi aliquam eu consequat blandit dignissim dolor. Nullam interdum sed etiam at id varius nunc. Consectetur id vitae purus. \n\nrhoncus volutpat velit ac bibendum nisi. Id ipsum amet eu sem porta auctor vestibulum. Mauris nunc enim est lorem velit velit. Justo tristique dictum euismod id eros lorem orci mauris. Aliquet in blandit nibh commodo praesent euismod a. Dolor eleifend quam tempus quispellentesque gravida. Sagittis vulputate egestas eget aliquet odio. Amet rutrum pellentesque aliquam tempus nibh. \n\nsollicitudin odio netus ac. Aenean vulputate morbi tempus gravida. Tincidunt turpis purus rutrum amet tincidunt. Ut nunc ut suspendisse nisi ut. Tellus proin rhoncus mauris a lacus. Vel viverra aliquet imperdiet eget integer netus. Commodo dignissim ultricies tellus viverra vulputate est vive tristique. Porttitor at hendrerit adipiscing phasellus facilisis vulputate. In varius cras imperdiet pretium in tellus tempus blandit imperdiet."
+                });
+              });
+            },
+            icon: Icon(Icons.feedback_outlined),
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(24),),
-            child: UserChatContainer()
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _aiController.aiInput.length,
+              itemBuilder: (context, index) {
+                if (index == 0 || index % 2 == 0) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(24),
+                    ),
+                    child: UserChatContainer(
+                      inputText:
+                          _aiController.aiInput[index]["user"].toString(),
+                    ),
+                  );
+                } else {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(24),
+                    ),
+                    child: AiChatContainer(
+                      inputText: _aiController.aiInput[index]["ai"].toString(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: ChatTextField(
+              textController: _aiController.textInputController,
+              hintText: "Enter Description",
+              aiResponse: aiResponse,
+              onTap: () async {
+                if (_aiController.textInputController.text.isNotEmpty) {
+                  setState(() {
+                    aiResponse = false;
+                    _aiController.aiInput
+                        .add({"user": _aiController.textInputController.text});
+                    _aiController.textInputController.clear();
+                  });
+                  await Future.delayed(Duration(seconds: 5));
+
+                  setState(() {
+                    aiResponse = true;
+                    _aiController.aiInput.add({
+                      "ai":
+                          "Lorem ipsum dolor sit amet consectetur. Phasellus leo felis ac ut a eu. Id tincidunt scelerisque malesuada etiam elit sodales sem amet suspendisse. Tortor vitae vulputate ipsum risus dignissim adipiscing lobortis. Porttitor volutpat dolor faucibus lobortis dis orci et et. Senectus id pulvinar odio. \n\nfermentum elementum tellus ut mauris. Fringilla pellentesque lorem neque volutpat. Eu morbi dui morbi aliquam eu consequat blandit dignissim dolor. Nullam interdum sed etiam at id varius nunc. Consectetur id vitae purus. \n\nrhoncus volutpat velit ac bibendum nisi. Id ipsum amet eu sem porta auctor vestibulum. Mauris nunc enim est lorem velit velit. Justo tristique dictum euismod id eros lorem orci mauris. Aliquet in blandit nibh commodo praesent euismod a. Dolor eleifend quam tempus quispellentesque gravida. Sagittis vulputate egestas eget aliquet odio. Amet rutrum pellentesque aliquam tempus nibh. \n\nsollicitudin odio netus ac. Aenean vulputate morbi tempus gravida. Tincidunt turpis purus rutrum amet tincidunt. Ut nunc ut suspendisse nisi ut. Tellus proin rhoncus mauris a lacus. Vel viverra aliquet imperdiet eget integer netus. Commodo dignissim ultricies tellus viverra vulputate est vive tristique. Porttitor at hendrerit adipiscing phasellus facilisis vulputate. In varius cras imperdiet pretium in tellus tempus blandit imperdiet."
+                    });
+                  });
+                  print(_aiController.aiInput[0]["user"]);
+                }
+              },
+            ),
+          )
+        ],
       ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      // floatingActionButton: FloatingActionButton(
-      //     backgroundColor: AppTheme.whiteColor,
-      //     elevation: 0,
-      //     child: Icon(
-      //       Icons.share_rounded,
-      //       color: AppTheme.shareColor,
-      //       size: getProportionateScreenWidth(31),
-      //     ),
-      //     onPressed: () {}),
     );
   }
 }
